5b178dbd7fe02974784e60e2ec7dac180f1e139e
Kiel Cipriaso <kielcipri@gmail.com>
2024-10-24 19:27:05 +0800

    crc-32 added

--- Lab 03 - Error Detection/CRC_Encoder.py
+++ Lab 03 - Error Detection/CRC_Encoder.py
@@ -1,18 +1,6 @@
 def xor(a, b):
     return ''.join('0' if a[i] == b[i] else '1' for i in range(1, len(b)))
 
-def longDivision(message, generator):
-    pick = len(generator)
-    tmp = message[:pick]
-    message = message + '0'*(keylen-1) 
-
-    while pick < len(message):
-        tmp = xor(generator if tmp[0] == '1' else '0'*pick, tmp) + message[pick]
-        pick += 1
-
-    tmp = xor(generator if tmp[0] == '1' else '0'*pick, tmp)
-    return tmp
-
 
 def shiftRegister(message, generator):
     m, g = int(message, 2) << (len(generator) - 1), int(generator, 2)
@@ -21,16 +9,34 @@ def shiftRegister(message, generator):
             m ^= (g << (len(message) - 1 - i))
     return format(m & ((1 << (len(generator) - 1)) - 1), f'0{len(generator) - 1}b')
 
+def longDivisionCRC(message, generator):
+    pick = len(generator)
+    message = message + '0' * (len(generator) - 1) 
+    tmp = message[:pick]
+
+    while pick < len(message):
+        tmp = xor(generator if tmp[0] == '1' else '0'*pick, tmp) + message[pick]
+        pick += 1
+
+    tmp = xor(generator if tmp[0] == '1' else '0'*pick, tmp)
+    return tmp
+
 if __name__ == "__main__":
     message = '1010011010'
     generator = '110101'
+    crc32Generator = '100000100110000010001110110110111'
     keylen = len(generator)
 
-    long_division_remainder = longDivision(message, generator)
+    long_division_remainder = longDivisionCRC(message, generator)
+    crc_32_checksum = longDivisionCRC(message, crc32Generator)
     #shift_register_remainder = shiftRegister(message, generator)
 
     print(f"Message in Binary: {message}")
     print(f"Generator Polynomial in Binary: {generator}")
+    print(f"Generator Polynomial in Binary (CRC-32): {crc32Generator}")
 
-    print(f"Remainder using long-hand division: {''.join(map(str, long_division_remainder))}")
+
+    print(f"\nRemainder using long-hand division: {''.join(map(str, long_division_remainder))}")
+    print(f"Remainder using CRC-32: {''.join(map(str, crc_32_checksum))}")
+    print(f"Encoded Message using CRC-32: 1010011010{''.join(map(str, crc_32_checksum))}")
     #print(f"Remainder using shift register: {''.join(map(str, shift_register_remainder))}")
