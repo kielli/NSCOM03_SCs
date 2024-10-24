d21069141e6358db40def75cc34d42ffe2d30b3d
Kiel Cipriaso <kielcipri@gmail.com>
2024-10-24 11:52:40 +0800

    CRC2

--- Lab 03 - Error Detection/CRC_Encoder.py
+++ Lab 03 - Error Detection/CRC_Encoder.py
@@ -1,13 +1,6 @@
 def xor(a, b):
     return ''.join('0' if a[i] == b[i] else '1' for i in range(1, len(b)))
 
-def shiftRegister(message, generator):
-    m, g = int(message, 2) << (len(generator) - 1), int(generator, 2)
-    for i in range(len(message)):
-        if m & (1 << (len(message) + len(generator) - 2 - i)):
-            m ^= (g << (len(message) - 1 - i))
-    return format(m & ((1 << (len(generator) - 1)) - 1), f'0{len(generator) - 1}b')
-
 def longDivision(message, generator):
     pick = len(generator)
     tmp = message[:pick]
@@ -20,14 +13,25 @@ def longDivision(message, generator):
     tmp = xor(generator if tmp[0] == '1' else '0'*pick, tmp)
     return tmp
 
+
+def shiftRegister(message, generator):
+    m, g = int(message, 2) << (len(generator) - 1), int(generator, 2)
+    for i in range(len(message)):
+        if m & (1 << (len(message) + len(generator) - 2 - i)):
+            m ^= (g << (len(message) - 1 - i))
+    return format(m & ((1 << (len(generator) - 1)) - 1), f'0{len(generator) - 1}b')
+
+
 if __name__ == "__main__":
     message = '1010011010'
     generator = '110101'
     keylen = len(generator)
 
     long_division_remainder = longDivision(message, generator)
+    shift_register_remainder = shiftRegister(message, generator)
 
     print(f"Message in Binary: {message}")
     print(f"Generator Polynomial in Binary: {generator}")
 
     print(f"Remainder using long-hand division: {''.join(map(str, long_division_remainder))}")
+    print(f"Remainder using shift register: {''.join(map(str, shift_register_remainder))}")
